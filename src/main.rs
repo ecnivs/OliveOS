816ae5766be03f3dfb595372bcdaed6bffc513f3
Vince Swu <vinceswu88@gmail.com>
2025-02-05 20:50:15 +0530

    Double Faults

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,12 @@ pub extern "C" fn _start() -> ! {
 
     OliveOS::init();
 
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    stack_overflow();
+
     x86_64::instructions::interrupts::int3();
     #[cfg(test)]
     test_main();
